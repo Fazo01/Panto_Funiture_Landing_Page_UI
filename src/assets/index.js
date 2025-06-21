efacef6a33568d25ad2453118f281fa6fde41dd0
Fazo01 <fg7829098@gmail.com>
2025-06-21 11:14:49 +0500

    Add Image Assets

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+import facebook from "./001-facebook.png";
+import twitter from "./003-twitter.png";
+import instagram from "./004-instagram.png";
+import AnjayChar from "./Anjay Chair.png";
+import Bag from "./Bag.png";
+import BaltsarChain from "./Baltsar Chair.png";
+import BangUpinBG from "./Bang Upin_BG.png";
+import BangUpin from "./Bang Upin.png";
+import Experience from "./Experience.png";
+import Fivestar from "./Five_star.png";
+import Fourstar from "./four_star.png";
+import IbukSukijan from "./Ibuk Sukijan.png";
+import IbukSukijanBG from "./Ibuk Sukijan_BG.png";
+import IconNextLink from "./icons_form-next-link.png";
+import IconSearch from "./icons_search.png";
+import LandingSection from "./LandingSection.png";
+import Logo from "./Logo.png";
+import Material_01 from "./Material_01.png";
+import Material_02 from "./Material_02.png";
+import Material_03 from "./Material_03.png";
+import MpokInaBG from "./Mpok Ina_BG.png";
+import MpokIna from "./Mpok Ina.png";
+import NyantuyChair from "./Nyantuy Chair.png";
+import PantoLogo from "./Panto.png";
+import Plus from "./plus.png";
+import SakariasArmchair from "./Sakarias Armchair.png";
+export {
+  facebook,
+  twitter,
+  instagram,
+  AnjayChar,
+  Bag,
+  BaltsarChain,
+  BangUpinBG,
+  BangUpin,
+  Experience,
+  Fivestar,
+  Fourstar,
+  IbukSukijan,
+  IbukSukijanBG,
+  IconNextLink,
+  IconSearch,
+  LandingSection,
+  Logo,
+  Material_01,
+  Material_02,
+  Material_03,
+  MpokInaBG,
+  MpokIna,
+  NyantuyChair,
+  PantoLogo,
+  Plus,
+  SakariasArmchair,
+};
